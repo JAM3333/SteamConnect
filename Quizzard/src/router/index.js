97bb38444b7b9b2605de10cee6a4a81db690bb04
KptnFishy <nils.miessgang@gmail.com>
2024-04-05 10:05:57 +0200

    Login page basic logic

--- Quizzard/src/router/index.js
+++ Quizzard/src/router/index.js
@@ -29,7 +29,7 @@ const routes = [
       },
       {
         path: '/login',
-        name: 'loginScreen',
+        name: 'Login',
         component: () => import('@/views/loginScreen.vue'),
       },
     ],
