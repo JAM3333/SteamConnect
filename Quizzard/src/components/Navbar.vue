ac1dacc28f32e3eaeb0bd9890f2cceed417091f4
KptnFishy <nils.miessgang@gmail.com>
2024-05-03 11:14:56 +0200

    epic login system update work now

--- Quizzard/src/components/Navbar.vue
+++ Quizzard/src/components/Navbar.vue
@@ -15,7 +15,7 @@
       <v-divider></v-divider>
       <v-list density="compact" nav>
         <v-list-item prepend-icon="mdi-account-alert" title="Login/Sign Up" value="Login" v-if="!loggedIn" :to="{path: '/login'}"></v-list-item>
-        <v-list-item prepend-avatar="../assets/logo.png" title="{this.Username}" value="Account" v-if="loggedIn">
+        <v-list-item prepend-avatar="../assets/logo.png" :title="this.Username" value="Account" v-if="loggedIn">
           <v-icon @click="logout()" style="position: absolute; left: 210px; right: 0; bottom: 8px; " color="error" v-if="loggedIn">mdi-logout</v-icon>
         </v-list-item>
       </v-list>
@@ -24,17 +24,44 @@
 </template>
 
 <script>
+import axios from 'axios';
  export default {
   name: 'Navbar',
   data() {
     return {
-      Username: 'fish',
+      Username: '',
+      Email: '',
       loggedIn: false,
+      Token: localStorage.getItem('token')
     };
   },
+  mounted() {
+    this.startupFunc();
+  },
   methods: {
+    async startupFunc() {
+      if (this.Token) {
+        console.log(localStorage.getItem('token'))
+        await axios.post("http://" + import.meta.env.VITE_SERVER_IP + ":" + import.meta.env.VITE_SERVER_PORT + "/loginrequest", {Token: this.Token})
+              .then((response) => {
+                console.log("answer from server:", response.data);
+                this.Username = response.data[0].Username;
+                this.Email = response.data[0].Email;
+                this.loggedIn = true;
+              })
+              .catch((error) => {
+                console.log("error recieved");
+                console.error("Error with the GET request:", error);
+              });
+      }
+      else {
+        localStorage.setItem('token', '')
+        this.loggedIn = false;
+      }
+    },
     logout() {
-      this.loggedIn = !this.loggedIn;
+      localStorage.setItem('token', '')
+      this.loggedIn = false;
     },
 },
 };
